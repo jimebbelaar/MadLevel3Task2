8ca6188ecea5644cef450932443c05e29a0f3812
Jim Ebbelaar <jim.ebbelaar@gmail.com>
2020-09-30 16:53:23 +0200

    Adding 1 value to portal

--- app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.madlevel3task2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -41,8 +43,18 @@ class PortalsFragment : Fragment() {
             DividerItemDecoration(context,
                 DividerItemDecoration.VERTICAL)
         )
-//        observeAddPortalResult()
-//        createItemTouchHelper().attachToRecyclerView(rvPortals)
+        observeAddPortalResult()
+    }
+    private fun observeAddPortalResult() {
+        setFragmentResultListener(REQ_PORTAL_KEY) { key, bundle ->
+            bundle.getString(BUNDLE_PORTAL_KEY)?.let {
+                val portal = Portal(it)
+
+                portals.add(portal)
+                portalAdapter.notifyDataSetChanged()
+            } ?: Log.e("ReminderFragment", "Request triggered, but empty reminder text!")
+
+        }
     }
 }
 
